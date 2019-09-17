b01c0c334b366835ed5f2594a6cf63cd01a7cf79
Status100 <bluemagicuk@yahoo.co.uk>
2019-09-17 12:28:41 +0100

    update modules in server and json package file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const port = process.env.port || 3000,
     bodyparser = require("body-parser"),
     mongoose = require("mongoose"),
-    express = require("express"),
-    app = express();
+    path = require("path");
+
+(express = require("express")), (app = express());
 
 app.set("view engine", "ejs");
 
@@ -13,18 +14,16 @@ app.use(
 );
 //this is a way of using, requiring and running at the same time.
 //It needs 3 options, the secret can be literally anything, it is used to code and decode web sessions
-app.use(
-    require("express-session")({
-        secret: "Eat my shorts",
-        resave: false,
-        saveUninitialized: false
-    })
-);
+// app.use(
+//     require("express-session")({
+//         secret: "Eat my shorts",
+//         resave: false,
+//         saveUninitialized: false
+//     })
+// );
 
-app.use(express.static(__dirname + "/CSS/"));
-app.use(express.static(__dirname + "/Fonts/"));
-app.use(express.static(__dirname + "/Media/"));
-app.use(express.static(__dirname + "/JS/"));
+
+app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/lignumv", {
     useNewUrlParser: true
@@ -43,7 +42,7 @@ mongCon.on("disconnected", function () {
 // });
 
 app.get("/", function (req, res) {
-    res.render("main");
+    res.sendFile(path.join(__dirname + "/HTML/lignumIndex.html"));
 });
 
 // app.get("*", (req, res) => {
@@ -52,21 +51,22 @@ app.get("/", function (req, res) {
 //     });
 // });
 
-app.get("*", (req, res) => {
-    res.send("404");
-});
+// app.get("*", (req, res) => {
+//   res.send("404");
+// });
 
 app.listen(port, function () {
     console.log("You rockin now on port " + port);
+    console.log(__dirname + "/HTML/lignumIndex.html");
 });
 
-var http = require("http");
-var servz = http.createServer(function (req, res) {
-    res.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-    res.end("Sup bitches?");
-});
+// var http = require("http");
+// var servz = http.createServer(function (req, res) {
+//     res.writeHead(200, {
+//         "Content-Type": "text/plain"
+//     });
+//     res.end("Sup bitches?");
+// });
 
 // var customerSchema = new mongoose.Schema({
 //     name: {
@@ -88,7 +88,6 @@ var servz = http.createServer(function (req, res) {
 // var Customer = mongoose.model("Customer", customerSchema);
 // this makes a model based on the customer schema with methods on it so that you can do Customer.find etc
 
-
 // Customer.create({
 //     name: {
 //         firstName: 'Marcia',
@@ -105,4 +104,4 @@ var servz = http.createServer(function (req, res) {
 //     }
 // });
 
-servz.listen(3001, "127.0.0.1");
+// servz.listen(3001, "127.0.0.1");
